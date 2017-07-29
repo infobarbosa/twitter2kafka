77f673b4aeea60646202ba5ad68884a755280b73
infobarbosa <infobarbosa@yahoo.com.br>
2017-07-29 00:23:34 -0300

    adição da validação de presença da hashtag.

--- src/main/java/com/infobarbosa/App.java
+++ src/main/java/com/infobarbosa/App.java
@@ -6,6 +6,12 @@ public class App {
 
   public static void main(String[] args) {
 
+  	if( args.length == 0 ){
+  		throw new RuntimeException("Hashtag nao informada.");
+  	}else{
+  		System.out.println("Buscando a hashtag " + args[0]);
+  	}
+
     try {
       TwitterService twitterService = TwitterService.getInstance();
       twitterService.run( args[0] );
